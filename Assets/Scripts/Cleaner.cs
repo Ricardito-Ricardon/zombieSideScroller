3a8d43e42d77e2be69b5a536c9dc2892be2726a6
Ricardito-Ricardon <rikielperro64@gmail.com>
2023-11-28 16:04:13 -0600

    MoreAdded

---
+++ Assets/Scripts/Cleaner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cleaner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerHealth playerDead = other.gameObject.GetComponent<PlayerHealth>();
+            playerDead.makeDead();
+        }
+        else Destroy(gameObject);
+    }
+}
