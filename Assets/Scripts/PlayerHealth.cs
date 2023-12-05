4de50505dc7a86dc4cfd0dea667e51792c408b28
Ricardito-Ricardon <rikielperro64@gmail.com>
2023-12-05 14:57:09 -0600

    HealthPickup

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -9,30 +10,57 @@ public class PlayerHealth : MonoBehaviour
 
     public GameObject playerDeathFX;
 
+    //HUD
+    public Slider playerHealthSlider;
+    public Image damagedScreen;
+    Color flashColor = new Color(255f, 255f, 255f, 1f);
+    float flashSpeed = 5f;
+    bool damaged = false;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = fullHealth;
+        playerHealthSlider.maxValue = fullHealth;
+        playerHealthSlider.value = currentHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //are we hurt?
+        if (damaged)
+        {
+            damagedScreen.color = flashColor;
+        }else
+        {
+            damagedScreen.color = Color.Lerp(damagedScreen.color, Color.clear, flashSpeed*Time.deltaTime);
+        }
+        damaged = false;
     }
 
     public void addDamage(float damage)
     {
         currentHealth -= damage;
+        playerHealthSlider.value = currentHealth;
+        damaged = true;
         if(currentHealth < 0)
         {
             makeDead();
         }
     }
 
+    public void addHealth(float health)
+    {
+        currentHealth += health;
+        if(currentHealth>fullHealth) currentHealth = fullHealth;
+        playerHealthSlider.value = currentHealth;
+    }
+
     public void makeDead()
     {
         Instantiate(playerDeathFX, transform.position, Quaternion.Euler(new Vector3(-90, 0, 0)));
+        damagedScreen.color = flashColor;
         Destroy(gameObject);
     }
 }
