88f73c74fcce4858c7dd6f1d717a5687e39a93fc
Ricardito-Ricardon <rikielperro64@gmail.com>
2023-12-08 15:13:59 -0600

    Before Serializing Fields

---
+++ Assets/Scripts/SoundRandomizer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundRandomizer : MonoBehaviour
+{
+    public AudioClip[] sounds;
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        source.clip = sounds[Random.Range(0, sounds.Length)];
+        source.PlayOneShot(source.clip);
+    }
+}
